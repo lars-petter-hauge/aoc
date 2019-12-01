b11c5f27d03b3a42d7998c416ec5d5e78fa3b875
Lars Petter <larsenhauge@gmail.com>
2019-12-01 15:34:56 +0100

    day1 2019

---
+++ 2019/day1/day1.py
@@ -0,0 +1,25 @@
+import math
+
+def parse_input(fname):
+    with open(fname) as f:
+        data = f.readlines()
+
+    data = [int(d) for d in data]
+    return data
+
+def calc_fuel_amount(mass):
+    return math.floor((mass/3)) - 2
+
+def calc_fuel_amount_recursive(mass):
+    if mass <= 5:
+        return 0
+    fuel_req = math.floor((mass/3)) - 2
+    return fuel_req + calc_fuel_amount_recursive(fuel_req)
+
+def main():
+    masses = parse_input("day1_input.txt")
+    fuel_amount = [calc_fuel_amount_recursive(m) for m in masses]
+    print(sum((fuel_amount)))
+
+if __name__ == "__main__":
+    main()
---
+++ 2019/day1/test_day1.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+from day1 import main, calc_fuel_amount, calc_fuel_amount_recursive
+
+
+
+class TestDay1(unittest.TestCase):
+    def test_main(self):
+        pass
+    
+    def test_fuel_amount(self):
+        self.assertEqual(2, calc_fuel_amount(12))
+        self.assertEqual(2, calc_fuel_amount(14))
+        self.assertEqual(654, calc_fuel_amount(1969))
+        self.assertEqual(33583, calc_fuel_amount(100756))
+    
+    def test_fuel_amount_recursive(self):
+        self.assertEqual(2, calc_fuel_amount_recursive(14))
+        self.assertEqual(966, calc_fuel_amount_recursive(1969))
+        self.assertEqual(50346, calc_fuel_amount_recursive(100756))
+
+if __name__ == "__main__":
+    unittest.main()
