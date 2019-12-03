93bd644660a99e581dc7abd23a9554a0024347dc
Lars Petter <larsenhauge@gmail.com>
2019-12-03 22:20:10 +0100

    Add day2

---
+++ 2019/python/day2/main.py
@@ -0,0 +1,32 @@
+import copy
+import itertools
+
+def parse_input(fname):
+    with open(fname) as f:
+        data = f.readlines()
+    return [int(x) for x in data[0].split(",")]
+
+def run(data):
+    i = 0
+    while data[i] != 99:
+        op, a, b, c = data[i:i+4]
+        if op == 1:
+            data[c] = data[a] + data[b]
+        elif op == 2:
+            data[c] = data[a] * data[b]
+        else:
+            raise NotImplementedError
+        i+=4
+    return data
+
+if __name__ == '__main__':
+    original_data = parse_input("day2/input.txt")
+
+    for x,y in itertools.permutations(range(99),2):
+        data = copy.copy(original_data)
+        data[1] = x
+        data[2] = y
+        data = run(data)
+        if data[0] == 19690720:
+            break
+    print("{},{}".format(data[1], data[2]))
---
+++ 2019/python/day2/test_main.py
@@ -0,0 +1,25 @@
+import unittest
+
+from day2.main import run
+
+class Test(unittest.TestCase):
+    def test_run(self):
+        data = [1,9,10,3,2,3,11,0,99,30,40,50]
+        expected = [3500,9,10,70,2,3,11,0,99,30,40,50]
+        self.assertEqual(expected, run(data))
+
+        data = [1,0,0,0,99]
+        expected = [2,0,0,0,99]
+        self.assertEqual(expected, run(data))
+
+        data = [2,3,0,3,99]
+        expected = [2,3,0,6,99]
+        self.assertEqual(expected, run(data))
+
+        data = [2,4,4,5,99,0]
+        expected = [2,4,4,5,99,9801]
+        self.assertEqual(expected, run(data))
+
+        data = [1,1,1,4,99,5,6,0,99]
+        expected = [30,1,1,4,2,5,6,0,99]
+        self.assertEqual(expected, run(data))
