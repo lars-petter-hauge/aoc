3bfbb94a1671c6a989d306b147f358de25f80118
Lars Petter Øren Hauge <lpha@equinor.com>
2019-12-03 15:14:14 +0100

    Remove whitespace

--- 2019/python/day1/test_day1.py
+++ 2019/python/day1/test_day1.py
@@ -8,13 +8,13 @@ from day1 import main, calc_fuel_amount, calc_fuel_amount_recursive
 class TestDay1(unittest.TestCase):
     def test_main(self):
         pass
-    
+
     def test_fuel_amount(self):
         self.assertEqual(2, calc_fuel_amount(12))
         self.assertEqual(2, calc_fuel_amount(14))
         self.assertEqual(654, calc_fuel_amount(1969))
         self.assertEqual(33583, calc_fuel_amount(100756))
-    
+
     def test_fuel_amount_recursive(self):
         self.assertEqual(2, calc_fuel_amount_recursive(14))
         self.assertEqual(966, calc_fuel_amount_recursive(1969))
