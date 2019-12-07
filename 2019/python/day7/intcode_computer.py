d4c69825b70470b4ffa9d0b68726ce32b28ed07c
Lars Petter Øren Hauge <larsenhauge@gmail.com>
2019-12-07 13:27:08 +0100

    Add day7 part2

--- 2019/python/day7/intcode_computer.py
+++ 2019/python/day7/intcode_computer.py
@@ -6,184 +6,191 @@ def parse_input(fname):
         data = f.readlines()
     return [int(x) for x in data[0].split(",")]
 
+class IntCodeComputer:
+    def __init__(self, program, phase_setting=None, yield_at_output=False):
+        self.program = program
+        self.yield_at_output = yield_at_output
+        self.pos = 0
+        self.completed = True
+        self.operation = None
 
-def add(intcodes, i, read_a_method, read_b_method, value=None):
-    a, b, c = intcodes[i:i+3]
-    value = read_a_method(intcodes, a) + read_b_method(intcodes, b)
-    intcodes[c] = value
+        self.phase_setting = phase_setting
+        self.value = None
+        self.phase_set = phase_setting is None
+        self.read_first_method = None
+        self.read_second_method = None
 
+        self.read_modes = {
+            0: self.from_position,
+            1: self.from_memory
+        }
 
-def multiply(intcodes, i, read_a_method, read_b_method, value=None):
-    a, b, c = intcodes[i:i+3]
-    value = read_a_method(intcodes, a) * read_b_method(intcodes, b)
-    intcodes[c] = value
+        self.operations = {
+            1: self.add,
+            2: self.multiply,
+            3: self.save,
+            4: self.output,
+            5: self.jump_if_true,
+            6: self.jump_if_false,
+            7: self.less_than,
+            8: self.equals,
+        }
 
-
-def save(intcodes, i, read_a_method, read_b_method, value=None):
-    pos = intcodes[i]
-    intcodes[pos] = value
-
-
-def output(intcodes, i, read_a_method, read_b_method, value=None):
-    pos = read_a_method(intcodes, i)
-    return intcodes[pos]
-
-
-def jump_if_true(intcodes, i, read_a_method, read_b_method, value=None):
-    a, b, c = intcodes[i:i+3]
-    a = read_a_method(intcodes, a)
-    b = read_b_method(intcodes, b)
-    if a:
-        return b
-
-
-def jump_if_false(intcodes, i, read_a_method, read_b_method, value=None):
-    a, b, c = intcodes[i:i+3]
-    a = read_a_method(intcodes, a)
-    b = read_b_method(intcodes, b)
-    if not a:
-        return b
-
-
-def less_than(intcodes, i, read_a_method, read_b_method, value=None):
-    a, b, c = intcodes[i:i+3]
-    a = read_a_method(intcodes, a)
-    b = read_b_method(intcodes, b)
-    if a<b:
-        intcodes[c] = 1
-    else:
-        intcodes[c] = 0
-
-
-def equals(intcodes, i, read_a_method, read_b_method, value=None):
-    a, b, c = intcodes[i:i+3]
-    a = read_a_method(intcodes, a)
-    b = read_b_method(intcodes, b)
-    if a==b:
-        intcodes[c] = 1
-    else:
-        intcodes[c] = 0
-
-
-def from_position(intcodes, i):
-    return intcodes[i]
-
-
-def from_memory(intcodes, i):
-    return i
-
-
-def parse_opcode(code):
-    code = str(code)
-
-    if len(code)==5:
-        a = int(code[0])
-        code = code[1:]
-    else:
-        a = 0
-
-    if len(code)==4:
-        b = int(code[0])
-        code = code[1:]
-    else:
-        b = 0
-
-    if len(code)==3:
-        c = int(code[0])
-        code = code[1:]
-    else:
-        c = 0
-
-    return a, b, c, int(code)
-
-mode = {
-    0: from_position,
-    1: from_memory
-}
-
-operations = {
-    1: add,
-    2: multiply,
-    3: save,
-    4: output,
-    5: jump_if_true,
-    6: jump_if_false,
-    7: less_than,
-    8: equals,
-}
-
-step_length = {
-    1: 3,
-    2: 3,
-    3: 1,
-    4: 1,
-    5: 2,
-    6: 2,
-    7: 3,
-    8: 3,
-}
-
-def run(intcodes, values=None):
-    i = 0
-    if values is None:
-        raise NotImplementedError
-
-    if not isinstance(values, list):
-        values = [values, values]
-
-    first, second = values
-    value = first
-    while intcodes[i] != 99:
-        _, b, c, op = parse_opcode(intcodes[i])
-        #print("code: {}, Performing operation: {}, mode: {}, mode: {}".format(intcodes[i], operations[op].__name__, mode[c].__name__, mode[b].__name__))
-        i += 1
-        output = operations[op](intcodes, i, mode[c], mode[b], value)
-        if op == 3:
-            value = second
-        if op == 4:
-            value = output
-        if op in [5, 6] and output is not None:
-            i = output
+    def run(self, input_signal):
+        self.completed = True
+        if not self.phase_set:
+            self.value = self.phase_setting
         else:
-            i += step_length[op]
+            self.value = input_signal            
 
-    return value
+        while self.program[self.pos] != 99:
+            self.update_mode()
+            self.operation()
+
+            if self.operation.__name__ == 'save':
+                if not self.phase_set:
+                    self.value = input_signal
+                    self.phase_set = True
+
+            if self.operation.__name__ == 'output':
+                if self.yield_at_output:
+                    self.completed = False
+                    break
+
+    def add(self):
+        a, b, c = self.program[self.pos:self.pos+3]
+        value = self.read_first_method(a) + self.read_second_method(b)
+        self.program[c] = value
+        self.pos += 3
+
+    def multiply(self):
+        a, b, c = self.program[self.pos:self.pos+3]
+        value = self.read_first_method(a) * self.read_second_method(b)
+        self.program[c] = value
+        self.pos += 3
+
+    def save(self):
+        pos = self.program[self.pos]
+        self.program[pos] = self.value
+        self.pos += 1
+
+    def output(self):
+        pos = self.read_first_method(self.pos)
+        self.value = self.program[pos]
+        self.pos += 1
+
+    def jump_if_true(self):
+        a, b, c = self.program[self.pos:self.pos+3]
+        a = self.read_first_method(a)
+        b = self.read_second_method(b)
+        if a:
+            self.pos = b
+        else:
+            self.pos += 2
+
+    def jump_if_false(self):
+        a, b, c = self.program[self.pos:self.pos+3]
+        a = self.read_first_method(a)
+        b = self.read_second_method(b)
+        if not a:
+            self.pos = b
+        else:
+            self.pos += 2
+
+    def less_than(self):
+        a, b, c = self.program[self.pos:self.pos+3]
+        a = self.read_first_method(a)
+        b = self.read_second_method(b)
+        if a < b:
+            self.program[c] = 1
+        else:
+            self.program[c] = 0
+        self.pos += 3
+
+    def equals(self):
+        a, b, c = self.program[self.pos:self.pos+3]
+        a = self.read_first_method(a)
+        b = self.read_second_method(b)
+        if a == b:
+            self.program[c] = 1
+        else:
+            self.program[c] = 0
+        self.pos += 3
+
+    def from_position(self, pos):
+        return self.program[pos]
+
+    def from_memory(self, pos):
+        return pos
+
+    def update_mode(self):
+        code = str(self.program[self.pos])
+
+        if len(code) == 5:
+            a = int(code[0])
+            code = code[1:]
+        else:
+            a = 0
+
+        if len(code) == 4:
+            b = int(code[0])
+            code = code[1:]
+        else:
+            b = 0
+        self.read_second_method = self.read_modes[b]
+
+        if len(code) == 3:
+            c = int(code[0])
+            code = code[1:]
+        else:
+            c = 0
+        self.read_first_method = self.read_modes[c]
+
+        op = int(code)
+        self.operation = self.operations[op]
+        self.pos += 1
+
+def run(intcodes, input_signal, phase_setting=None, pos=0, yield_at_output=False):
+    computer = IntCodeComputer(intcodes, phase_setting=phase_setting, yield_at_output=yield_at_output)
+    computer.run(input_signal)
+
+    return computer.program, computer.value, computer.pos, computer.completed
 
 def run_tests():
 
-    for value in range(-10,10):
+    for input_signal in range(-10,10):
         # Positional mode
-        assert (value == 8) == run([3,9,8,9,10,9,4,9,99,-1,8], values=value)
-        assert (value < 8) == run([3,9,7,9,10,9,4,9,99,-1,8], values=value)
+        assert (input_signal == 8) == run([3,9,8,9,10,9,4,9,99,-1,8], input_signal=input_signal)[1]
+        assert (input_signal < 8) == run([3,9,7,9,10,9,4,9,99,-1,8], input_signal=input_signal)[1]
 
         # Immediate mode
-        assert (value == 8) == run([3,3,1108,-1,8,3,4,3,99], values=value)
-        assert (value < 8) == run([3,3,1107,-1,8,3,4,3,99], values=value)
+        assert (input_signal == 8) == run([3,3,1108,-1,8,3,4,3,99], input_signal=input_signal)[1]
+        assert (input_signal < 8) == run([3,3,1107,-1,8,3,4,3,99], input_signal=input_signal)[1]
 
         # Jump mode
-        assert (value != 0) == run([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], values=value)
-        assert (value != 0) == run([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], values=value)
+        assert (input_signal != 0) == run([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], input_signal=input_signal)[1]
+        assert (input_signal != 0) == run([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], input_signal=input_signal)[1]
 
         # Larger example
         program = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
                    1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
                    999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
-        output = run(program, values=value)
+        output = run(program, input_signal=input_signal)[1]
 
-        if value < 8:
+        if input_signal < 8:
             assert output == 999
-        elif value == 8:
+        elif input_signal == 8:
             assert output == 1000
         else:
             assert output == 1001
 
     original_data = parse_input("day5/input.txt")
-    value = run(original_data, values=1)
-    assert value == 13210611
+    input_signal = run(original_data, input_signal=1)[1]
+    assert input_signal == 13210611
 
     original_data = parse_input("day5/input.txt")
-    value = run(original_data, values=5)
-    assert value == 584126
+    input_signal = run(original_data, input_signal=5)[1]
+    assert input_signal == 584126
 
 if __name__ == '__main__':
     run_tests()
--- 2019/python/day7/main.py
+++ 2019/python/day7/main.py
@@ -2,16 +2,38 @@ from copy import copy
 import itertools
 
 from intcode_computer import parse_input
-from intcode_computer import run as run_intcode_computer
+from intcode_computer import IntCodeComputer
 
 def run(controller_input, phase_settings):
     input_signal = 0
     for phase_setting in phase_settings:
-        input_signal = run_intcode_computer(copy(controller_input), values=[phase_setting, input_signal])
+        computer = IntCodeComputer(program=copy(controller_input),
+                                   phase_setting=phase_setting)
+        computer.run(input_signal)
+        input_signal = computer.value
     return input_signal
 
 
-def run_tests():
+def run_part2(controller_input, phase_settings):
+    computers = [
+        IntCodeComputer(program=copy(controller_input),
+                        phase_setting=phase,
+                        yield_at_output=True)
+            for phase in phase_settings
+    ]
+
+    completed = False
+    input_signal = 0
+    while not completed:
+        for computer in computers:
+            computer.run(input_signal)
+            input_signal = computer.value
+
+        completed = all(c.completed for c in computers)
+
+    return computers[0].value
+
+def run_tests_part1():
     controller_input = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
     assert 43210 == run(controller_input, phase_settings=[4,3,2,1,0])
 
@@ -23,10 +45,24 @@ def run_tests():
                         1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
     assert 65210 == run(controller_input, phase_settings=[1,0,4,3,2])
 
+def run_tests_part2():
+    controller_input = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+                        27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+    value = run_part2(controller_input, phase_settings=[9,8,7,6,5])
+    assert 139629729 == value
+
+    controller_input = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+                        -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+                        53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
+
+    value = run_part2(controller_input, phase_settings=[9,7,8,5,6])
+    assert 18216 == value
+
 if __name__ == '__main__':
     controller_input = parse_input("day7/input.txt")
-    run_tests()
+    run_tests_part1()
+    run_tests_part2()
     max_thruster = 0
-    for phase_settings in itertools.permutations([0,1,2,3,4], 5):
-        max_thruster = max(max_thruster, run(controller_input, phase_settings))
+    for phase_settings in itertools.permutations([5,6,7,8,9], 5):
+        max_thruster = max(max_thruster, run_part2(controller_input, phase_settings))
     print(max_thruster)
