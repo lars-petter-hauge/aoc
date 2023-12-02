58a7db571990f29e21ad33c4b6221e4f1e2f73f5
Lars Petter Hauge <larsenhauge@gmail.com>
2023-12-02 22:38:16 +0100

    Refactor aoc app to include terminal print

--- aoc_app/src/AoC_app.py
+++ aoc_app/src/AoC_app.py
@@ -37,12 +37,14 @@ class AocClient:
         # Preferably one would want to add "Accept": "application/json"
         # in the header to specify content type, but it does not look
         # like that is possible. Specify type by appending .json to url
-
+        url = f"{url}.json"
         # Author of AoC specifically asked to be personal in the request
         # (i.e. adding contact information to developer), as he could
         # potentially restrict anonymous IPs (likely if they misbehaved).
+        logger.debug(f"AoC URL:{url}")
+
         response = requests.get(
-            f"{url}.json",
+            url,
             headers={
                 "User-Agent": "https://github.com/lars-petter-hauge/aoc",
                 "From": "larsenhauge@gmail.com",
@@ -85,7 +87,24 @@ class SlackBotClient:
         if self.token:
             self._slack_client = WebClient(self.token)
 
+        self.post(
+            {
+                "text": " :christmas_tree: Hello, I am an Advent of Code leaderboard tracker - I'll be keeping you lot up to date :christmas_tree:",
+                "type": "mrkdwn",
+            }
+        )
+
+    def _format_slack_message(self, content):
+        timestamp, data = content
+        msg = (
+            f":christmas_tree: At {str(timestamp)}, *{data['name']}* gained "
+            f"{''.join([':star:']*int(data['star']))} for day *{data['day']}*"
+            ":christmas_tree:"
+        )
+        return {"text": msg}
+
     def post(self, content):
+        content = self._format_slack_message(content)
         if self._slack_client is not None:
             try:
                 result = self._slack_client.chat_postMessage(
@@ -141,13 +160,7 @@ class LeaderBoard:
         return new_entries
 
 
-def format_slack_message(content):
-    timestamp, data = content
-    msg = f":christmas_tree: At {str(timestamp)}, *{data['name']}* gained {''.join([':star:']*int(data['star']))} for day *{data['day']}* :christmas_tree:"
-    return {"text": msg}
-
-
-def monitor_and_post(aoc_client, slack_client, update_interval=60 * 5):
+def monitor_and_post(aoc_client, reporters, update_interval=60 * 5):
     logger.debug("Retrieving members")
     members = aoc_client.get_members()
     leader_board = LeaderBoard(members)
@@ -165,7 +178,8 @@ def monitor_and_post(aoc_client, slack_client, update_interval=60 * 5):
             )
             for update in dict(sorted(update_since_last.items())).items():
                 logger.debug(f"Posting the following update {update}")
-                slack_client.post(format_slack_message(update))
+                for reporter in reporters:
+                    reporter.post(update)
                 time.sleep(0.3)
             logger.info("Info posted")
         else:
@@ -175,6 +189,7 @@ def monitor_and_post(aoc_client, slack_client, update_interval=60 * 5):
 
 
 def setup_logger(verbosity):
+    level = logging.WARNING
     if verbosity == 0:
         level = logging.WARNING
     elif verbosity == 1:
@@ -195,6 +210,22 @@ def setup_logger(verbosity):
     logger.setLevel(level)
 
 
+class PrinterReporter:
+    def __init__(self):
+        print("Ready to print updates!")
+
+    def _format_message(self, content):
+        timestamp, data = content
+        msg = (
+            f"At {str(timestamp)}, {data['name']} gained "
+            f"star number {data['star']} for day {data['day']}"
+        )
+        return msg
+
+    def post(self, content):
+        print(self._format_message(content))
+
+
 def retrieve_value(config, name, err_msg=None, exit_on_failure=False):
     value = os.environ.get(name)
     if value is None:
@@ -204,8 +235,50 @@ def retrieve_value(config, name, err_msg=None, exit_on_failure=False):
     return value
 
 
+def setup_slack_client(slack_token=None, slack_channel_id=None, slack_bot_url=None):
+    if slack_token is not None:
+        if slack_channel_id is None:
+            logger.warning(
+                "Slack token provided, but did not find channel. Please provide SLACK_CHANNEL in config or as an environment variable"
+            )
+            return None
+        return SlackBotClient(token=slack_token, channel=slack_channel_id)
+    elif slack_bot_url is not None:
+        logger.info("Slack token not provided, falling back on SLACK_BOT_ENTRY")
+        return SlackBotClient(url=slack_bot_url)
+    return None
+
+
+config_help = """File path to the config file.
+
+The config file must contain information to fetch and post Advent of Code updates.
+
+The application expects the following content:
+
+{
+    AOC_SESSION: <session_id>
+    LEADER_BOARD: <leader_board url>
+    SLACK_TOKEN: <slack token>
+    SLACK_CHANNEL: <channel to post to>
+    SLACK_BOT_ENTRY: <slack channel complete url (alternative)>
+}
+
+The AOC session id can be retrieved through developer console in the browser
+(there is no good way of doing this otherwise)
+
+The Leader board url can be copied from the url when looking at the leader board of interest.
+It is the ID at the end of the url.
+
+It is possible to connect to slack in two different ways;
+ - slack token and slack channel
+ - slack url (which contains a secret in the url)
+
+It is not required to provide slack identification. Updates from AoC will be published in the terminal regardless.
+"""
+
+
 @click.command()
-@click.option("--config_file", required=True, help="File path to config file")
+@click.option("--config_file", required=True, help=config_help)
 @click.option("--year", required=True, help="The year the leaderboard will be tracked")
 @click.option(
     "-v",
@@ -213,14 +286,14 @@ def retrieve_value(config, name, err_msg=None, exit_on_failure=False):
     help="Sets the logger level. Default is warning. Adding multiple flags increases verbosity. I.e -v=info, -vv=debug",
     count=True,
 )
-def aoc_tracker(config_file, year, verbose):
+@click.option("--interval", help="Seconds between each update", default=60 * 5)
+def aoc_tracker(config_file, year, verbose, interval):
     """Advent of Code leaderboard tracker
 
     Allows for tracking a leaderboard and posting to a slack bot"""
     setup_logger(verbose)
     logger.info("Initiating Advent of Code Leaderboard Tracker")
     config = load_config(config_file)
-
     session = retrieve_value(
         config,
         "AOC_SESSION",
@@ -233,40 +306,26 @@ def aoc_tracker(config_file, year, verbose):
         year=year,
     )
 
-    slack_token = retrieve_value(
-        config,
-        "SLACK_TOKEN",
-    )
-    slack_channel_id = retrieve_value(
-        config,
-        "SLACK_CHANNEL",
-    )
-    slack_client = None
-    if slack_token is not None:
-        if slack_channel_id is None:
-            sys.exit(
-                "Slack token provided, but did not find channel. Please provide SLACK_CHANNEL in config or as an environment variable"
-            )
-        slack_client = SlackBotClient(token=slack_token, channel=slack_channel_id)
-    else:
-        logger.info("Slack token not provided, falling back on SLACK_BOT_ENTRY")
-        slack_bot_url = retrieve_value(
+    reporters = [PrinterReporter()]
+    slack_client = setup_slack_client(
+        slack_token=retrieve_value(
+            config,
+            "SLACK_TOKEN",
+        ),
+        slack_channel_id=retrieve_value(
+            config,
+            "SLACK_CHANNEL",
+        ),
+        slack_bot_url=retrieve_value(
             config,
             "SLACK_BOT_ENTRY",
-            "The slack bot url must be provided. Please set SLACK_BOT_ENTRY either as environment variable, or in config file, and try again",
-            exit_on_failure=True,
-        )
-        slack_client = SlackBotClient(url=slack_bot_url)
-
-    slack_client.post(
-        {
-            "text": " :christmas_tree: Hello, I am an Advent of Code leaderboard tracker - I'll be keeping you lot up to date :christmas_tree:",
-            "type": "mrkdwn",
-        }
+        ),
     )
+    if slack_client is not None:
+        reporters.append(slack_client)
 
     try:
-        monitor_and_post(aoc_client, slack_client)
+        monitor_and_post(aoc_client, reporters, interval)
     except KeyboardInterrupt:
         sys.exit("Advent of Code tracker completed")
 
