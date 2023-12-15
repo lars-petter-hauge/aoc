3d68567ad6cc7a63291d9d3fd77a1e48b15edbbe
Lars Petter Hauge <larsenhauge@gmail.com>
2023-12-15 15:32:10 +0100

    Add day8 2023 part 2

--- 2023/python/day8/main.py
+++ 2023/python/day8/main.py
@@ -19,6 +19,19 @@ ZZZ = (ZZZ, ZZZ)
 """
 
 
+TEST_DATA_PART_TWO = """LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
+
+
 def load_input(fname):
     with open(fname) as fh:
         return fh.readlines()
@@ -35,24 +48,39 @@ def parse_input(lines):
     return instructions, nodemap
 
 
-def solve(instructions, nodemap):
+def reduce_nodemap(nodemap):
+    result = {}
+    for key, val in nodemap.items():
+        result[key[:2]] = {"L": val["L"][:2], "R": val["R"][:2]}
+    return result
+
+
+def solve(instructions, nodemap, key="AAA", goal=None):
+    goal = goal or ["ZZZ"]
     n_steps = 0
-    key = "AAA"
     while True:
         instr = instructions[n_steps % len(instructions)]
         key = nodemap[key][instr]
         n_steps += 1
-        if key == "ZZZ":
+        if key in goal:
             break
     return n_steps
 
 
-instructions, nodemap = parse_input(TEST_DATA.split("\n"))
-print(solve(instructions, nodemap))
+def solve_part_two(instructions, nodemap):
+    start_keys = [key for key in nodemap.keys() if key.endswith("A")]
+    goal_keys = [key for key in nodemap.keys() if key.endswith("Z")]
+    iters = [solve(instructions, nodemap, key, goal_keys) for key in start_keys]
+    lcm = math.lcm(*iters)
+    return lcm
+
 
 instructions, nodemap = parse_input(TEST_DATA_TWO.split("\n"))
 print(solve(instructions, nodemap))
 
+instructions, nodemap = parse_input(TEST_DATA_PART_TWO.split("\n"))
+print(solve_part_two(instructions, nodemap))
 instructions, nodemap = parse_input(load_input("input.txt"))
 
 print(solve(instructions, nodemap))
+print(solve_part_two(instructions, nodemap))
